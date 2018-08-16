33db5c7b0740c2dfefa10ca114443e5fb749cc8f
Younes Charfaoui <mxcsyounes@gmail.com>
2018-08-16 16:15:28 +0100

    Design the main interface, need icon for more #7

--- ArcherApp/app/src/main/java/me/mxcsyounes/archernotebook/activities/MainActivity.java
+++ ArcherApp/app/src/main/java/me/mxcsyounes/archernotebook/activities/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 import me.mxcsyounes.archernotebook.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        MobileAds.initialize(this, getResources().getString(R.string.app_id_ad_test));
+
+        AdView mAdView = findViewById(R.id.add_view);
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 
 }
