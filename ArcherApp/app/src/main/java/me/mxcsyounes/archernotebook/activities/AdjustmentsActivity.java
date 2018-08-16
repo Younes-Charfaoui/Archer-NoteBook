7f2f40d8ca04224338cb6b1010ade33441854567
Younes Charfaoui <mxcsyounes@gmail.com>
2018-08-16 16:46:59 +0100

    Adding Activities without any design #8

---
+++ ArcherApp/app/src/main/java/me/mxcsyounes/archernotebook/activities/AdjustmentsActivity.java
@@ -0,0 +1,33 @@
+package me.mxcsyounes.archernotebook.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import me.mxcsyounes.archernotebook.R;
+
+public class AdjustmentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adjustments);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.add_adjust_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
--- ArcherApp/app/src/main/java/me/mxcsyounes/archernotebook/activities/MainActivity.java
+++ ArcherApp/app/src/main/java/me/mxcsyounes/archernotebook/activities/MainActivity.java
@@ -1,8 +1,13 @@
 package me.mxcsyounes.archernotebook.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -10,7 +15,7 @@ import com.google.android.gms.ads.MobileAds;
 
 import me.mxcsyounes.archernotebook.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +26,48 @@ public class MainActivity extends AppCompatActivity {
 
         MobileAds.initialize(this, getResources().getString(R.string.app_id_ad_test));
 
-        AdView mAdView = findViewById(R.id.add_view);
+        AdView mAdView = findViewById(R.id.ad_view);
 
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+
+        findViewById(R.id.adjust_card_view).setOnClickListener(this);
+        findViewById(R.id.score_card_view).setOnClickListener(this);
+        findViewById(R.id.bow_setting_card_view).setOnClickListener(this);
+        findViewById(R.id.more_card_view).setOnClickListener(this);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_setting:
+                // TODO: 16-Aug-18 adding action.
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.adjust_card_view:
+                startActivity(new Intent(this, AdjustmentsActivity.class));
+                break;
+            case R.id.score_card_view:
+                startActivity(new Intent(this, ScoresActivity.class));
+                break;
+            case R.id.bow_setting_card_view:
+                startActivity(new Intent(this, BowSettingActivity.class));
+                break;
+            case R.id.more_card_view:
+                Toast.makeText(this, R.string.more_toast, Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
