777753d953fbe35e8cc01b3ce9fb09975a1bb7cf
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-05 22:51:36 +0100

    Adding color to precious scores card views #63

--- ArcherApp/app/src/main/java/me/mxcsyounes/archernotebook/adapters/PreviousScoresAdapter.kt
+++ ArcherApp/app/src/main/java/me/mxcsyounes/archernotebook/adapters/PreviousScoresAdapter.kt
@@ -1,6 +1,8 @@
 package me.mxcsyounes.archernotebook.adapters
 
 import android.content.Context
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -13,10 +15,11 @@ import me.mxcsyounes.archernotebook.model.ColorScore
 import java.text.SimpleDateFormat
 import java.util.*
 
-class PreviousScoresAdapter(context: Context, private val listener: PreviousScoreListener) : RecyclerView.Adapter<PreviousScoresAdapter.PreviousScoreViewHolder>() {
+class PreviousScoresAdapter(val context: Context, private val listener: PreviousScoreListener) : RecyclerView.Adapter<PreviousScoresAdapter.PreviousScoreViewHolder>() {
 
     private val layoutInflater = LayoutInflater.from(context)
     private var scoresList: List<ScoreSheet>? = null
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PreviousScoreViewHolder {
         val view = layoutInflater.inflate(R.layout.score_list_item, parent, false)
@@ -27,9 +30,15 @@ class PreviousScoresAdapter(context: Context, private val listener: PreviousScor
 
     override fun onBindViewHolder(holder: PreviousScoreViewHolder, position: Int) {
         with(scoresList?.get(position)) {
+            val colors = getColor(this)
             holder.scoreTv.text = this?.sumOfAll().toString()
             val format = SimpleDateFormat("E dd MMM", Locale.getDefault())
             holder.scoreDateTv.text = format.format(this?.date)
+
+            holder.scoreDateTv.setTextColor(ContextCompat.getColor(context, colors.textColor))
+            holder.scoreTv.setTextColor(ContextCompat.getColor(context, colors.textColor))
+            holder.scoresCard.setCardBackgroundColor(ContextCompat.getColor(context, colors.mainColor))
+            holder.scoresDark.setBackgroundColor(ContextCompat.getColor(context, colors.darkColor))
         }
     }
 
@@ -41,6 +50,8 @@ class PreviousScoresAdapter(context: Context, private val listener: PreviousScor
     inner class PreviousScoreViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val scoreTv: TextView = view.scoreSheetItemScore
         val scoreDateTv: TextView = view.scoreSheetItemDate
+        val scoresCard: CardView = view as CardView
+        val scoresDark: View = view.view
 
         init {
             view.setOnClickListener {
@@ -53,27 +64,27 @@ class PreviousScoresAdapter(context: Context, private val listener: PreviousScor
         fun onClickScoreItem(scoreSheet: ScoreSheet)
     }
 
-    fun getColor(scoreSheet: ScoreSheet): ColorScore {
-        val average : Double = scoreSheet.sumOfAll().toDouble() / scoreSheet.numberOfArrows.toDouble()
+    fun getColor(scoreSheet: ScoreSheet?): ColorScore {
+        val average: Double = scoreSheet?.sumOfAll()?.toDouble()!! / scoreSheet.numberOfArrows.toDouble()
 
-        return when (average) {
-            in 0..2 -> {
-                ColorScore(1, 1, 1)
+        return when (average.toInt()) {
+            in 0 until 2 -> {
+                ColorScore(R.color.black, R.color.whiteTwoDark, R.color.whiteTwo)
             }
-            in 2..4 -> {
-                ColorScore(1, 1, 1)
+            in 2 until 4 -> {
+                ColorScore(R.color.whiteTwo, R.color.blackDark, R.color.black)
             }
-            in 4..6 -> {
-                ColorScore(1, 1, 1)
+            in 4 until 6 -> {
+                ColorScore(R.color.whiteTwo, R.color.blueDark, R.color.blue)
             }
-            in 6..8 -> {
-                ColorScore(1, 1, 1)
+            in 6 until 8 -> {
+                ColorScore(R.color.whiteTwo, R.color.redDark, R.color.red)
             }
-            in 8..10 -> {
-                ColorScore(1, 1, 1)
+            in 8 until 10 -> {
+                ColorScore(R.color.whiteTwo, R.color.yellowDark, R.color.yellow)
             }
             else -> {
-                ColorScore(1, 1, 1)
+                ColorScore(R.color.whiteTwo, R.color.colorPrimaryDark, R.color.colorPrimary)
             }
         }
     }
