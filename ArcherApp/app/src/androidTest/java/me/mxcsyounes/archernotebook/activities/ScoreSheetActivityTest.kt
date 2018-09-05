f3e42bf4e1a6eebbba28c967b2042a2f4aa674b1
Younes Charfaoui <mxcsyounes@gmail.com>
2018-09-05 09:21:31 +0100

    Testing ScoresheetActivity UI #56

--- ArcherApp/app/src/androidTest/java/me/mxcsyounes/archernotebook/activities/ScoreSheetActivityTest.kt
+++ ArcherApp/app/src/androidTest/java/me/mxcsyounes/archernotebook/activities/ScoreSheetActivityTest.kt
@@ -1,6 +1,7 @@
 package me.mxcsyounes.archernotebook.activities
 
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
@@ -20,7 +21,31 @@ class ScoreSheetActivityTestIns {
 
     @Test
     fun test() {
+        onView(withId(R.id.arrowOne))
+                .check(matches(isDisplayed()))
 
-        
+        onView(withId(R.id.imageTen))
+                .perform(click())
+
+        onView(withId(R.id.scoreSumVoletTv))
+                .check(matches(withText("10")))
+
+        onView(withId(R.id.imageX))
+                .perform(click())
+
+        onView(withId(R.id.scoreSumVoletTv))
+                .check(matches(withText("20")))
+
+        onView(withId(R.id.imageNine))
+                .perform(click())
+
+        onView(withId(R.id.scoreSumVoletTv))
+                .check(matches(withText("29")))
+
+        onView(withId(R.id.arrowTwo))
+                .perform(click())
+
+        onView(withId(R.id.scoreSumVoletTv))
+                .check(matches(withText("19")))
     }
 }
